b4fc288daaa2ee7d76265d1322a356bf850c2164
askar <mr.askar1984@gmail.com>
2023-09-02 09:51:07 +0300

    sd

--- resources/views/landing_ar.blade.php
+++ resources/views/landing_ar.blade.php
@@ -73,7 +73,8 @@
 
                     <div class="tel-menu">
                         <p class="mbr-text mbr-fonts-style display-7">
-                            اتصل بنا <a href="#" class="text-success">(+20) 110 010 1626</a>
+                        اتصل بنا <a href="#" class="text-success">(+20) 110 010 1626</a>
+
                         </p>
                     </div>
                 </div>
